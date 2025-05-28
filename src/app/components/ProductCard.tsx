475845345f7d661ec5bac51271577c3b1ed687e3
Andres Pluska <andres.pluska@verusen.com>
2025-05-28 17:46:10 -0500

    resolving conflicts

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import "./ProductCard.css";
 import Image from "next/image";
-=======
-import './ProductCard.css';
->>>>>>> c10241c (card)
 
 type Product = {
   id: string;
@@ -15,17 +11,7 @@ type Product = {
 export default function ProductCard({ product }: { product: Product }) {
   return (
     <div className="product-card">
-<<<<<<< HEAD
-      <Image
-        src={product.image}
-        alt={product.name}
-        className="product-image"
-        width={100}
-        height={100}
-      />
-=======
       <img src={product.image} alt={product.name} className="product-image" />
->>>>>>> c10241c (card)
       <h3 className="product-name">{product.name}</h3>
       <p className="product-price">${product.price.toFixed(2)}</p>
     </div>
